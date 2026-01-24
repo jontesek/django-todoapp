e39901551503b208e2941b02673b8eb7404c9fbe
Jonáš Petrovský <jontesek@gmail.com>
2026-01-24 19:50:31 +0100

    feat: Add DB model and init migration

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    # Basic attributes
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False, db_index=True)
+    # Relationships
+    parent = models.ForeignKey("Task", related_name="subtasks", on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey("auth.User", related_name="tasks", on_delete=models.CASCADE)
+    # Metadata
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title

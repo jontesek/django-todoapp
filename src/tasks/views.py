43baa485f275229d83681ffc4397d68b2d5f72a0
Jonáš Petrovský <jontesek@gmail.com>
2026-01-25 21:05:21 +0100

    feat: Add structlog, update readme

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,68 +1,82 @@
 from collections import defaultdict
 
-from django_filters import rest_framework as filters
+import structlog
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
-from rest_framework import permissions
-from rest_framework.response import Response
+from django_filters import rest_framework as filters
+from rest_framework import generics, permissions
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .filters import TaskFilter
-from .serializers import TaskSerializer, TaskTreeSerializer
 from .models import Task
 from .permissions import IsOwner
+from .serializers import TaskSerializer, TaskTreeSerializer
+
+logger = structlog.get_logger(__name__)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def health(request):
-    """Health check"""
+    """Health check."""
     data = {"status": "ok"}
-    return Response(data) 
+    return Response(data)
 
 
 class TaskList(generics.ListCreateAPIView):
     """List all tasks, create new task."""
+
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = TaskFilter
 
     def get_queryset(self):
-        return Task.objects.filter(user=self.request.user)
+        tasks = Task.objects.filter(user=self.request.user)
+        logger.info(
+            "task_list.get_queryset.result",
+            user_id=self.request.user.pk,
+            user_task_count=len(tasks),
+        )
+        return tasks
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        
+        logger.info("task_list.perform_create.saved_new_task")
+
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     """Get task by ID, update values, delete task."""
+
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
 
 class RootTaskList(generics.ListAPIView):
-    """List all tasks without parent"""
+    """List all tasks without parent."""
+
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user, parent=None)
-    
+
 
 class SubtasksList(generics.ListAPIView):
-    """List all direct subtasks of the given task"""
+    """List all direct subtasks of the given task."""
+
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def get_queryset(self):
-        task_id = self.kwargs.get('pk')
+        task_id = self.kwargs.get("pk")
         get_object_or_404(Task, pk=task_id, user=self.request.user)
         return Task.objects.filter(user=self.request.user, parent=task_id)
-    
+
 
 class SubtasksTreeList(generics.ListAPIView):
     """List all subtasks of the given task in a hierarchical tree structure."""
+
     serializer_class = TaskTreeSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwner]
     # Disable pagination to make sure the whole tree is returned
@@ -70,7 +84,7 @@ class SubtasksTreeList(generics.ListAPIView):
 
     def get_queryset(self):
         # Get task ID from the URL
-        task_id = self.kwargs.get('pk')
+        task_id = self.kwargs.get("pk")
 
         # Check the task exists and is accessible by the user
         get_object_or_404(Task, pk=task_id, user=self.request.user)
@@ -80,14 +94,14 @@ class SubtasksTreeList(generics.ListAPIView):
 
     def get_serializer_context(self):
         # Get all user tasks
-        user_tasks = Task.objects.filter(user=self.request.user).only('id', 'parent_id')
-        
+        user_tasks = Task.objects.filter(user=self.request.user).only("id", "parent_id")
+
         # Build a mapping of parent_id -> list of children
         children_map = defaultdict(list)
         for task in user_tasks:
-            children_map[task.parent_id].append(task) # type: ignore
-        
+            children_map[task.parent_id].append(task)  # type: ignore
+
         # Put the map in context so the serializer can use it without queries
         context = super().get_serializer_context()
-        context['children_map'] = children_map
+        context["children_map"] = children_map
         return context
